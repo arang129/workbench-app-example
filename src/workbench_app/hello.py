e93ceb7472728402ec4d7d879984f8831542901d
Matus Kosut <matus.kosut@ntnu.no>
2023-04-17 16:35:45 +0200

    fix order

--- src/workbench_app/hello.py
+++ src/workbench_app/hello.py
@@ -8,9 +8,10 @@ root_url_prefix = os.getenv('FLASK_APP_URL_PREFIX', '/')
 app = Flask(__name__)
 root_bp = Blueprint("root", __name__, template_folder='templates')
 
-app.register_blueprint(root_bp, url_prefix=root_url_prefix)
-
 
 @root_bp.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+app.register_blueprint(root_bp, url_prefix=root_url_prefix)
